619742d8b040ec585c46dbc6c2af5f87522c4080
jeffarbar <jeffarbar@yahoo.com.br>
2020-06-05 00:54:10 -0300

    mais

--- api.sh
+++ api.sh
@@ -1,1 +1,1 @@
-docker run --name catalogo-dados-backend --network somenetwork  --link elasticsearch -p 8092:8092 -d jeffersonfarias/catalogo-dados-backend:1.0.0
+docker run --name catalogo-dados-backend --link elasticsearch  --network somenetwork  -p 8092:8092 -d jeffersonfarias/catalogo-dados-backend:1.0.0
